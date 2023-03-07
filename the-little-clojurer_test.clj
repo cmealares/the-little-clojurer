f2ccede717b9de32120f32f186726cb068fcb7d8
Christophe Mealares <4078480+cmealares@users.noreply.github.com>
2023-03-07 09:46:20 +0100

    Separate tests from code.

---
+++ the-little-clojurer_test.clj
@@ -0,0 +1,220 @@
+(ns the-little-clojurer.core-test
+  (:require [clojure.test :refer :all]
+            [the-little-clojurer.core :refer :all]))
+
+;; ---------------------------------------------------------------------------
+;; 2. Do It, Do It Again, and Again, and Again...
+;; ---------------------------------------------------------------------------
+
+(deftest chap2test
+  (testing "lat?"
+    (is (= true (lat? '(Jack Sprat could eat no chicken fat))))
+    (is (= false (lat? '((Jack) Sprat could eat no chicken fat))))
+    (is (= true (lat? ()))))
+
+  (testing "member?"
+    (is (= false (member? 'tea ())))
+    (is (= true (member? 'tea '(coffee tea or milk))))
+    (is (= false (member? 'poached '(fried eggs and scrambled eggs))))))
+
+;; ---------------------------------------------------------------------------
+;; 3. Cons the Magnificent
+;; ---------------------------------------------------------------------------
+
+(deftest chap3test
+  (testing "rember"
+    (are [x y] (= x y)
+      (rember 'mint ()) ()
+      (rember 'mint '(lamb chops and mint jelly)) '(lamb chops and jelly)
+      (rember 'mint '(lamb chops and mint flavored mint jelly)) '(lamb chops and flavored mint jelly)
+      (rember 'toast '(bacon lettuce and tomato)) '(bacon lettuce and tomato)
+      ))
+
+  (testing "firsts"
+    (are [x y] (= x y)
+      (firsts ()) ()
+      (firsts '((apple peach pumpkin) (plum pear cherry) (grape raisin pea) (bean carrot eggplant))) '(apple plum grape bean)
+      ))
+
+  (testing "insertR"
+    (are [x y] (= x y)
+      (insertR 'topping 'fudge '(ice cream with fudge for dessert)) '(ice cream with fudge topping for dessert)
+      (insertR 'e 'd '(a b c d f g h)) '(a b c d e f g h)))
+
+  (testing "insertL"
+    (is (= (insertL 'e 'f '(a b c d f g h)) '(a b c d e f g h))))
+
+  (testing "subst"
+    (is (= (subst 'topping 'fudge '(ice cream with fudge for dessert)) '(ice cream with topping for dessert))))
+
+  (testing "subst2"
+    (is (= (subst2 'vanilla 'chocolate 'banana '(banana ice cream with chocolate topping))
+           '(vanilla ice cream with chocolate topping))))
+
+  (testing "multirember"
+    (is (= (multirember 'cup '(coffee cup tea cup and hick cup)) '(coffee tea and hick))))
+
+  (testing "multiinsertR"
+    (is (= (multiinsertR 'x 'a '(a b c a d)) '(a x b c a x d))))
+
+  (testing "multiinsertL"
+    (is (= (multiinsertL 'x 'a '(a b c a d)) '(x a b c x a d))))
+
+   (testing "multisubst"
+    (is (= (multisubst 'x 'a '(a b c a d)) '(x b c x d)))) )
+
+;; ---------------------------------------------------------------------------
+;; 4. Numbers Games
+;; ---------------------------------------------------------------------------
+
+(deftest chap4test
+  (testing "o+"
+    (is (= 5 (o+ 2 3))))
+
+  (testing "o-"
+    (is (= 11 (o- 14 3))))
+
+  (testing "tup?"
+    (is (= true (tup? '(2 11 3))))
+    (is (= false (tup? '(1 2 8 apple 4 3))))
+    (is (= false (tup? '(3 (7 4) 13 9))))
+    (is (= true (tup?  '()))))
+
+  (testing "addtup"
+    (is (= 18 (addtup '(3 5 2 8)))))
+
+  (testing "o*"
+    (is (= 15 (o* 5 3))))
+
+  (testing "tup+"
+    (is (= '(6 9) (tup+ '(2 3) '(4 6))))
+    (is (= '(7 13 8 1) (tup+ '(3 7 8 1) '(4 6)))))
+
+  (testing "o>"
+    (is (= false (o> 12 133)))
+    (is (= true (o> 120 11)))
+    (is (= false (o> 3 3))))
+
+  (testing "o<"
+    (is (= true (o< 4 6)))
+    (is (= false (o< 8 3)))
+    (is (= false (o< 3 3))))
+
+  (testing "o="
+    (is (= true (o= 4 4)))
+    (is (= false (o= 8 3)))
+    (is (= false (o= 3 8))))
+
+  (testing "exp"
+    (is (= 1 (exp 1 1)))
+    (is (= 8 (exp 2 3)))
+    (is (= 125 (exp 5 3))))
+
+  (testing "quotient"
+    (is (= 3 (quotient 15 4))))
+
+  (testing "length"
+    (is (= 6 (length '(hotdogs with mustard sauerkraut and pickles)))))
+
+  (testing "pick"
+    (is (= 'lasagna (pick 1 '(lasagna spaghetti ravioli macaroni meatball))))
+    (is (= 'macaroni (pick 4 '(lasagna spaghetti ravioli macaroni meatball)))))
+
+  (testing "rempick"
+    (is (= '(hotdogs with mustard) (rempick 3 '(hotdogs with hot mustard)))) )
+
+  (testing "no-nums"
+    (is (= '(pears prunes dates) (no-nums '(5 pears 6 prunes 9 dates)))))
+
+  (testing "occur"
+    (is (= 0 (occur 'a '(b c d))))
+    (is (= 2 (occur 'a '(b a c d a)))))
+
+  (testing "one?"
+    (is (= false (one? 0)))
+    (is (= true (one? 1)))
+    (is (= false (one? 5)))) )
+
+;; ---------------------------------------------------------------------------
+;; 5. Oh My Gawd: It's Full of Stars
+;; ---------------------------------------------------------------------------
+
+(deftest chap4test
+  (testing "rember*"
+    (is (= (rember* 'sauce '(((tomato sauce)) ((bean) sauce) (and ((flying)) sauce)))
+           '(((tomato)) ((bean)) (and ((flying))))
+           )))
+
+  (testing "insertR*"
+    (is (= (insertR* 'roast 'chuck '((how much (wood))
+                                     could
+                                     ((a (wood) chuck))
+                                     (((chuck)))
+                                     (if (a) ((wood chuck)))
+                                     could chuck wood))
+           '((how much (wood))
+             could
+             ((a (wood) chuck roast))
+             (((chuck roast)))
+             (if (a) ((wood chuck roast)))
+             could chuck roast wood) )))
+
+  (testing "occur*"
+    (is (= 5 (occur* 'banana '((banana)
+                               (split ((((banana ice)))
+                                       (cream (banana))
+                                       sherbet))
+                               (banana)
+                               (bread)
+                               (banana brandy))))))
+
+  (testing "subst*"
+    (is (= (subst* 'orange 'banana '((banana)
+                                     (split ((((banana ice)))
+                                             (cream (banana))
+                                             sherbet))
+                                     (banana)
+                                     (bread)
+                                     (banana brandy)))
+           '((orange)
+             (split ((((orange ice)))
+                     (cream (orange))
+                     sherbet))
+             (orange)
+             (bread)
+             (orange brandy))
+           )))
+
+  (testing "insertL*"
+    (is (= (insertL* 'pecker 'chuck '((how much (wood))
+                                      could
+                                      ((a (wood) chuck))
+                                      (((chuck)))
+                                      (if (a) ((wood chuck)))
+                                      could chuck wood))
+           '((how much (wood))
+             could
+             ((a (wood) pecker chuck))
+             (((pecker chuck)))
+             (if (a) ((wood pecker chuck)))
+             could pecker chuck wood)) ))
+
+  (testing "member*"
+    (is (= true (member* 'chips '((potato) (chips ((with) (chips))))))))
+
+  (testing "leftmost"
+    (is (= 'potato (leftmost '((potato) (chips ((with) (chips))))))))
+
+  (testing "eqlist?"
+    (is (= true (eqlist? '(strawberry ice cream) '(strawberry ice cream))))
+    (is (= false (eqlist? '(beef ((sausage)) (and (soda))) '(beef ((salami)) (and (soda))) )))
+    (is (= true (eqlist? '(beef ((sausage)) (and (soda))) '(beef ((sausage)) (and (soda))) ))) )
+
+  (testing "rember2"
+    (is (= '(a d) (rember2 '(a b) '(a (a b) d)) '(a d)))
+    (is (= '(a c) (rember2 '((b)) '(a ((b)) c))) '(a c))
+    (is (= '(((a))) (rember2 '(a) '( ( (a) )))) )  ))
+
+;; ---------------------------------------------------------------------------
+;; 6. Shadows
+;; ---------------------------------------------------------------------------
